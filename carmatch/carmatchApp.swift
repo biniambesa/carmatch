960c356e48b98735835c82c45ebbef6426d09d07
biniambesa <b.hagos17@gmail.com>
2022-10-21 11:28:07 -0700

    Initial Commit

---
+++ carmatch/carmatchApp.swift
@@ -0,0 +1,17 @@
+//
+//  carmatchApp.swift
+//  carmatch
+//
+//  Created by Sogah Mainib on 10/21/22.
+//
+
+import SwiftUI
+
+@main
+struct carmatchApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
